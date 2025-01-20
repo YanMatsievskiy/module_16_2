0dc3c10b37c81f284f7713f21ce03bea2853e8ba
Matsievskiy <ian.m@mail.ru>
2025-01-20 23:49:32 +0500

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -11,20 +11,16 @@ async def main_page() -> dict:
 async def admin() -> dict:
     return {"message": "Вы вошли как администратор"}
 
-@app.get("/user/{user_id}")
-async def user_(user_id: str) -> dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
-
 @app.get('/user')
 async def user_info(username: str, age: int) -> dict:
     return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
 
 @app.get("/user/{id}")
 async def get_page(id: int = Path(ge=1, le=100, description='Enter User ID', example='1')) -> dict:
-    return {"message": f"Your {id}"}
+    return {"message": f"Вы вошли как пользователь № {id}"}
 
 @app.get("/user/{username}/{age}")
 async def get_page(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
                                                  example='UrbanUser')],
                    age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]) -> dict:
-    return {"message": f"Hello {username} {age}"}
+    return {"message": f"Привет {username} {age}"}
